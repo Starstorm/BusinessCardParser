fd383dc0a8d2c6d0220815ee85f93cf7c91704cd
Starstorm <joshlee.grad@gmail.com>
2019-10-05 01:14:26 -0400

    Update BusinessCardParser.py

--- BusinessCardParser/BusinessCardParser.py
+++ BusinessCardParser/BusinessCardParser.py
@@ -37,3 +37,15 @@ class BusinessCardParser(object):
             if email_re and email == "UNKNOWN":
                 email = email_re.group().strip()
         return email, phone
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+    parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
