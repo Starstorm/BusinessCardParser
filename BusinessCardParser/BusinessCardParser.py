9acc3de4dd442ba20c5351e47b550020eba70491
Starstorm <joshlee.grad@gmail.com>
2019-10-05 01:04:05 -0400

    Create BusinessCardParser.py

---
+++ BusinessCardParser/BusinessCardParser.py
@@ -0,0 +1,39 @@
+class BusinessCardParser(object):
+    def __init__(self):
+        self.phone_regex = '(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'
+        self.email_regex = """(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+        self.name_db = names_dataset.NameDataset()
+        self.name_ner = spacy.load('en_core_web_sm')
+        
+    def getContactInfo(self, document):
+        name = self.extractName(document)
+        email, phone = self.extratEmailPhone(document)
+        return ContactInfo(name, phone, email)
+    
+    def extractName(self, document):
+        name = "UNKNOWN"
+        entities = self.name_ner(document)
+        people = [str(elem) for elem in entities.ents if elem.label_ == "PERSON" and str(elem).count(" ") > 0]
+        if len(people) == 1:
+            name = people[0].replace("\n","").strip()
+        elif len(people) > 1:
+            for person in people:
+                first_name = person.split()[0].strip()
+                if self.name_db.search_first_name(first_name):
+                    name = person
+                    break
+        return name
+    
+    def extractEmailPhone(self, document):
+        email = "UNKNOWN"
+        phone = "UNKNOWN"
+        document_lines = document.split("\n")
+        for line in document_lines:
+            phone_re = re.search(self.phone_regex,line)
+            email_re = re.search(self.email_regex,line)
+            if phone_re and phone == "UNKNOWN" and not line.startswith(("Fax","fax","FAX")):
+                raw_phone_number = phone_re.group().strip()
+                phone = re.sub('[^0-9]','', raw_phone_number)
+            if email_re and email == "UNKNOWN":
+                email = email_re.group().strip()
+        return email, phone
