db1cfc89ef3df59ba998fea16d102534d862baee
Starstorm <joshlee.grad@gmail.com>
2019-10-05 01:02:10 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='BusinessCardParser',
+    version='1.0.0',
+    packages=['names_dataset','spacy'],
+    long_description=open('README.txt').read(),
+)
