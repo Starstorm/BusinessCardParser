6a236fb7002fa1bb967b6473fbed53dd29a2e6b8
Starstorm <joshlee.grad@gmail.com>
2019-10-06 16:36:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='BusinessCardParser',
     version='1.0.0',
-    packages=find_packages()
+    packages=find_packages(),
     long_description=open('README.md').read(),
 )
