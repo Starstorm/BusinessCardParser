3af6441a63bd22310c1465aebb2a2a030fd88d66
Starstorm <joshlee.grad@gmail.com>
2019-10-06 16:42:27 -0400

    Update BusinessCardParser.py

--- BusinessCardParser/BusinessCardParser.py
+++ BusinessCardParser/BusinessCardParser.py
@@ -37,6 +37,25 @@ class BusinessCardParser(object):
             if email_re and email == "UNKNOWN":
                 email = email_re.group().strip()
         return email, phone
+
+class ContactInfo(object):
+    def __init__(self, name, phone_number, email_address):
+        self.name = name
+        self.phone_number = phone_number
+        self.email_address = email_address
+        
+    def __str__(self):
+        return "Name: " + self.name + "\nPhone: " + self.phone_number + "\nEmail: " + self.email_address
+    
+    def getName(self):
+        return self.name
+    
+    def getPhoneNumber(self):
+        return self.name
+    
+    def getEmailAddress(self):
+        return self.email_address
+    
 if __name__ == '__main__':
     import argparse
 
