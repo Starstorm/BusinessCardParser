51949bae51c43633a218da3b063556e5cdecbad6
Starstorm <joshlee.grad@gmail.com>
2019-10-06 16:13:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name='BusinessCardParser',
     version='1.0.0',
-    packages=['names_dataset','spacy'],
+    install_requires=['names_dataset', 'spacy'],
     long_description=open('README.md').read(),
 )
