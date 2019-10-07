f3caa9fe5ee2f6eb85e8f8bc8f9668e10c16365c
Starstorm <joshlee.grad@gmail.com>
2019-10-06 21:23:06 -0400

    Update testing.py

--- tests/testing.py
+++ tests/testing.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from BusinessCardParser import ContactInfo, BusinessCardParser
+import BusinessCardParser
 import unittest
-
 
 class TestSuite(unittest.TestCase):
     """Test cases."""
