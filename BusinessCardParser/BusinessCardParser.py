ad77755399e0b36201d5d886dedd4dc985c046e0
Starstorm <joshlee.grad@gmail.com>
2019-10-06 22:06:18 -0400

    Update BusinessCardParser.py

--- BusinessCardParser/BusinessCardParser.py
+++ BusinessCardParser/BusinessCardParser.py
@@ -27,8 +27,11 @@ class BusinessCardParser(object):
     """
 
     def __init__(self):
+        import spacy, names_dataset, re, sys
+        from ContactInfo import ContactInfo
         self.phone_regex = '(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'
         self.email_regex = """(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+        
     def getContactInfo(self, document):
         # Clean the document
         document = self.cleanDoc(document)
@@ -90,8 +93,7 @@ class BusinessCardParser(object):
 # Execution begins here
 if __name__ == '__main__':
     # Import required modules
-    import argparse, spacy, re, names_dataset, sys
-    from ContactInfo import ContactInfo
+    import argparse
     
     # Configure ArgumentParser for command line interface
     ap = argparse.ArgumentParser()
