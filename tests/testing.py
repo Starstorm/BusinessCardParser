2bf102f05260d98cbfbd3fb390c307021997e4b5
Starstorm <joshlee.grad@gmail.com>
2019-10-06 23:43:41 -0400

    Update testing.py

--- tests/testing.py
+++ tests/testing.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import unittest
 import sys, os, re
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -8,8 +7,16 @@ class TestSuite(unittest.TestCase):
     """Test cases."""
 
     def test_parser(self):
+        business_card = """ASYMMETRIK LTD
+        Mike Smith
+        Senior Software Engineer
+        (410)555-1234
+        msmith@asymmetrik.com"""
         my_parser = BusinessCardParser()
-		
+        self.assertIsNotNone(my_parser)
+        cleaned_card = my_parser.cleanDoc(business_card)
+        self.assertIsInstance(cleaned_card, str)
+		 
     def test_contact(self):
         my_info = ContactInfo("Arthur Wilson","17035551259","awilson@abctech.com")
         self.assertIsInstance(my_info.getName(), str)
