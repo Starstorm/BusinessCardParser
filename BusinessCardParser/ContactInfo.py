dcf1cd801dc701d2536b99efcf2aee8ca997cd8e
Starstorm <joshlee.grad@gmail.com>
2019-10-06 21:07:25 -0400

    Update ContactInfo.py

--- BusinessCardParser/ContactInfo.py
+++ BusinessCardParser/ContactInfo.py
@@ -1,6 +1,5 @@
 class ContactInfo(object):
-"""
-    A class used to represent the ContactInfo object
+"""A class used to represent the ContactInfo object
     ...
     Attributes
     ----------
@@ -19,9 +18,7 @@ class ContactInfo(object):
     String getEmailAddress()
         Basic getter method for individual's email address.
     String __str__()
-        Define stringification for the ContactInfo object. Stringifies the ContactInfo object as specified in the instructions provided at https://asymmetrik.com/programming-challenges/.
-"""
-
+        Define stringification for the ContactInfo object. Stringifies the ContactInfo object as specified in the instructions provided at https://asymmetrik.com/programming-challenges."""
     def __init__(self, name, phone_number, email_address):
         self.name = name
         self.phone_number = phone_number
