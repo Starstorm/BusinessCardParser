a93ce726501d280c49584f8883c32d99b1e6bfb4
Starstorm <joshlee.grad@gmail.com>
2019-10-06 21:19:51 -0400

    Update testing.py

--- tests/testing.py
+++ tests/testing.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import sys
+import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from BusinessCardParser import ContactInfo, BusinessCardParser
 import unittest
