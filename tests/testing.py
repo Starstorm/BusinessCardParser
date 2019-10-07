3b07b120824458badc38617a198638b89bed4295
Starstorm <joshlee.grad@gmail.com>
2019-10-06 22:26:20 -0400

    Update testing.py

--- tests/testing.py
+++ tests/testing.py
@@ -8,10 +8,31 @@ class TestSuite(unittest.TestCase):
     """Test cases."""
 
     def test_parser(self):
-        my_info = BusinessCardParser()
+        my_parser = BusinessCardParser()
+        my_info = my_parser.getContactInfo("""
+            Arthur Wilson
+            Software Engineer
+            Decision & Security Technologies
+            ABC Technologies
+            123 North 11th Street
+            Suite 229
+            Arlington, VA 22209
+            Tel: +1 (703) 555-1259
+            Fax: +1 (703) 555-1200
+            awilson@abctech.com
+		""")
+        self.assertEqual("Arthur Wilson", my_info.getName())
+        self.assertEqual("17035551259", my_info.getPhoneNumber())
+        self.assertEqual("awilson@abctech.com", my_info.getEmailAddress())
 		
     def test_contact(self):
-        my_info = ContactInfo("Derp","Derp","Derp")
+        my_info = ContactInfo("Arthur Wilson","17035551259","awilson@abctech.com")
+        self.assertIsInstance(str, my_info.getName())
+        self.assertIsInstance(str, my_info.getPhoneNumber())
+        self.assertIsInstance(str, my_info.getEmailAddress())
+        self.assertEqual("Arthur Wilson", my_info.getName())
+        self.assertEqual("17035551259", my_info.getPhoneNumber())
+        self.assertEqual("awilson@abctech.com", my_info.getEmailAddress())
 
 if __name__ == '__main__':
 	unittest.main()
