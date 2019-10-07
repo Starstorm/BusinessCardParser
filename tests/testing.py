dcb22bfa5ab13b33c1f6e4622191ee46e170a0a2
Starstorm <joshlee.grad@gmail.com>
2019-10-06 21:17:52 -0400

    Update testing.py

--- tests/testing.py
+++ tests/testing.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from BusinessCardParser.BusinessCardParser import BusinessCardParser
-from BusinessCardParser.ContactInfo import ContactInfo
+from BusinessCardParser import ContactInfo, BusinessCardParser
 import unittest
 
 
@@ -10,7 +9,8 @@ class TestSuite(unittest.TestCase):
     """Test cases."""
 
     def test_thoughts(self):
-        pass
+        my_info = ContactInfo("Derp","Derp","Derp")
+        print(str(my_info))
 
 if __name__ == '__main__':
     unittest.main()
